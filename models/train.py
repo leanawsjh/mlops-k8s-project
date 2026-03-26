9e1da12a1931736548a01da8a124c82dcabb20c1
javedhassans <javedhassans@outlook.com>
2026-03-26 15:46:20 +0530

    added mlflow to dependecies

--- models/train.py
+++ models/train.py
@@ -1,15 +1,16 @@
 from sklearn.datasets import fetch_california_housing
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor 
 from sklearn.model_selection import train_test_split
 import joblib
 import os
+import argparse
 
 data = fetch_california_housing()
-X, y = data.data, data.target
+X, y = data.dat a, data.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-model = LinearRegression()
+model = RandomForestRegressor()
 model.fit(X_train, y_train)
 
 score = model.score(X_test, y_test)
